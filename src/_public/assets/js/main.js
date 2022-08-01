80242845bdda10c7a429e546c7c6f2136c295a75
ttdat4 <dh51702456@student.stu.edu.vn>
2022-08-01 20:58:49 +0700

    up

---
+++ src/_public/assets/js/main.js
@@ -0,0 +1,11 @@
+
+
+
+$(document).ready(function(){
+    $('.l-slidewrap').slick({
+        slidesToShow: 1,
+        accessibility: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+    });
+  });
